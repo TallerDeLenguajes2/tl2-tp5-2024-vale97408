23926bec0226060e045202ed83eebe81193fa1dd
vale97408 <vale97408@gmail.com>
2025-01-21 00:51:28 -0300

    Creo el proyecto

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace tl2_tp5_2024_vale97408;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
